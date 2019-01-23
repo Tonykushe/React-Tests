0e8b6eebc5acbbe54ca4281a06e2fcc676cf8f5c
Tony Mugendi <mugendi.tony26@gmail.com>
2019-01-23 15:34:51 +0300

    Setup Enzyme

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,5 +3,8 @@ import ReactDOM from 'react-dom'
 import App from '../App'
 
 it('shows a comment box', () => {
-    
+    const div = document.createElement('div')
+
+    ReactDOM.render(<App />, div)
+    expect(div.innerHTML).toContain('Comment Box')
 })
