3b12ae5542be8f26864f41c8cbabc71bfa6ccb06
Tony Mugendi <mugendi.tony26@gmail.com>
2019-01-27 21:31:11 +0300

    expecting component instance

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import App from '../App'
 import { shallow } from "enzyme";
-import CommentBox from '../CommentBox';
-import CommentList from '../CommentList';
+import App from 'components/App'
+import CommentBox from 'components/CommentBox'
+import CommentList from 'components/CommentList';
 
+// Test that the CommentBox component exist in App
 it('shows a comment box', () => {
-
     const wrapped = shallow(<App />)
     expect(wrapped.find(CommentBox).length).toEqual(1)
 });
 
-
+// Test that the CommentList component exist in App
 it('show a comment list', () => {
     const wrapped = shallow(<App />)
     expect(wrapped.find(CommentList).length).toEqual(1)
